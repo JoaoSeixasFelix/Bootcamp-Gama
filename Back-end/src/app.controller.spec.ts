f05ccf70602411916c60d4b7860af32044a11a45
mateus.santos6 <mateus.santos@finderup.com.br>
2022-05-11 21:34:35 -0300

    wip: initiating back-end

---
+++ Back-end/src/app.controller.spec.ts
@@ -0,0 +1,44 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import * as supertest from 'supertest';
+import { SuperTest, Test as AgentTest } from 'supertest';
+import { INestApplication } from '@nestjs/common';
+
+let app: INestApplication;
+let request: SuperTest<AgentTest>;
+
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    app = module.createNestApplication();
+    await app.init();
+
+    appController = app.get<AppController>(AppController);
+    request = supertest(app.getHttpServer());
+  });
+
+  describe('root', () => {
+    it('app controller should exists', () => {
+      expect(appController).toBeTruthy();
+    });
+  });
+
+  describe('Controller', () => {
+    it('should return message', async () => {
+      await request
+        .get('/')
+        .expect(200)
+        .then((res) => {
+          expect(res.body.message).toBe('API Blacklist CPF');
+          expect(res.body.code).toBe(200);
+        });
+    });
+  });
+});
---
+++ Back-end/src/app.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Response } from 'express';
+import { AppService } from './app.service';
+
+@ApiTags('Default')
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('/')
+  @ApiOperation({ summary: 'Initial route' })
+  @ApiResponse({
+    status: 200,
+    description: 'Initial route',
+  })
+  initial(@Res() res: Response) {
+    return res.status(HttpStatus.OK).json({
+      message: 'API Blacklist CPF',
+      code: HttpStatus.OK,
+    });
+  }
+}
