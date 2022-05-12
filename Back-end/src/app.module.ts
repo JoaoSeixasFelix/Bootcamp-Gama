8291b84f15668a9197c94f11d94722a8b6f9dd9f
mateus.santos6 <mateus.santos@finderup.com.br>
2022-05-12 19:37:58 -0300

    wip: config swagger

--- Back-end/src/app.module.ts
+++ Back-end/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(process.env.MONGODB)],
   controllers: [AppController],
   providers: [AppService],
 })
--- Back-end/src/main.ts
+++ Back-end/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configSwagger: Omit<OpenAPIObject, 'paths'> = new DocumentBuilder()
+    .setTitle('Back-end - BootCamp Gama')
+    .setDescription('Back-end')
+    .setVersion('1.0')
+    .addTag('Back-end')
+    .build();
+
+  const documentSwagger: OpenAPIObject = SwaggerModule.createDocument(
+    app,
+    configSwagger,
+  );
+  SwaggerModule.setup('API', app, documentSwagger);
+
   await app.listen(3030);
 }
 bootstrap();
