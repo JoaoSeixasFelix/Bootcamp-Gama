478ca6886229c17a532cb5185407754636008dad
João Paulo <joaopaulo_seixas@outlook.com>
2022-05-30 17:20:46 -0300

    Update Project - Version 13 - Fix a bugs, and create a Context API.

---
+++ contexts/Context.ts
@@ -0,0 +1,13 @@
+import { createContext, useState } from "react";
+
+
+export const UsersContext = createContext();
+
+export function usersContextProvider ({children}){
+    const [] = useState();
+
+return(
+    UsersContext
+)
+
+}
