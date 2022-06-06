e14bc20528d937c2280f020806e325168dc7dac6
João Paulo <joaopaulo_seixas@outlook.com>
2022-06-06 01:35:57 -0300

    create first version lading page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from "next";
+import LadingPage from "./ladingpage";
 import SignIN from "./signin";
 
 const Home: NextPage = () => {
   return (
     <div>
-      <SignIN />
+      <LadingPage />
     </div>
   );
 };
